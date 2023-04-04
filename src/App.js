7eb8643a20eabc41075a66b35a8874671ba5d335
AllisterF1 <allytheforbes@gmail.com>
2023-04-04 18:33:35 +0100

    week 5 react submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleNewTodoAdd = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { text: newTodo.trim(), checked: false }]);
+      setNewTodo("");
+    }
+  };
+
+  const handleTodoCheck = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].checked = !newTodos[index].checked;
+    setTodos(newTodos);
+  };
+
+  const handleTodoEdit = (index, newText) => {
+    if (index === editIndex) {
+      const newTodos = [...todos];
+      newTodos[index].text = newText.trim();
+      setTodos(newTodos);
+      setEditIndex(-1);
+    }
+  };
+
+  const handleTodoDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleEditClick = (index) => {
+    setEditIndex(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>To-Do List</h1>
+      <div className="new-todo">
+        <input
+          type="text"
+          placeholder="Enter new to-do item"
+          value={newTodo}
+          onChange={handleNewTodoChange}
+        />
+        <button onClick={handleNewTodoAdd}>Add</button>
+      </div>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <div key={index} className="todo-item">
+            <input
+              
+              type="checkbox"
+              checked={todo.checked}
+              onChange={() => handleTodoCheck(index)}
+            />
+            {todo.checked ? (
+            <span className="todo-done">{todo.text}</span>
+   
+            ) : (
+              editIndex === index ? (
+                <input
+                  className="todo-edit"
+                  type="text"
+                  value={todo.text}
+                  onChange={(event) => handleTodoEdit(index, event.target.value)}
+                />
+              ) : (
+                <span onClick={() => handleTodoCheck(index)}>{todo.text}</span>
+              )
+            )}
+
+
+            {editIndex !== index && (
+              <button onClick={() => handleEditClick(index)}>Edit</button>
+            )}
+            <button onClick={() => handleTodoDelete(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
